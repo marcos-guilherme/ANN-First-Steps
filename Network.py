fe00136169824d96c6565b1fa14b23fb3e103b9c
marcos <marcos_oliveira2@discente.ufg.br>
2024-02-12 23:52:19 -0300

    feedforward_sigmoid#2

--- Network.py
+++ Network.py
@@ -8,4 +8,11 @@ class Rede(object):
         self.vies = [np.random.randn(y, 1) for y in sizes[1:]]
         self.pesos = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
+    def feedforward(self, a):
+        for v, p in zip(self.vies, self.pesos):
+            a = sigmoid(np.dot(p, a) + v)
+        return a
+    
 
+def sigmoid(x):
+    return 1.0/(1.0 + np.exp(-x))
