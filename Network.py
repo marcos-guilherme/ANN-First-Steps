b5a566de79c5359585cf2db32fbdf6078eebd1be
marcos <marcos_oliveira2@discente.ufg.br>
2024-02-14 14:00:25 -0300

    comments#4

--- Network.py
+++ Network.py
@@ -17,21 +17,26 @@ class Rede(object):
 
     #Essa função é responsável por receber um vetor de ativação (vetor com os valores de ativação de neurônios)
     #a partir deste vetor de ativação calculamos, com base nos pesos da camada atual, o novo vetor de ativação
-    #que será passado para a próxima camada
+    #que será passado para a próxima camada, lembrando que a função de ativação usada é a sigmoide.
     def feedforward(self, a):
         for v, p in zip(self.vies, self.pesos):
             a = sigmoid(np.dot(p, a) + v)
         return a
     
+    #É usada uma variante do método de minimizar a função de custo, onde 
+    #selecionamos mini-lotes aleatórios do conjunto de treinamento para ajustar os parâmetros
+    #da rede, ao invés de passar por todos os dados de treinamento.
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
 
 
         if test_data: n_test = len(test_data)
         n = len(training_data)
         
-        for j in xrange(epochs):
+    #Para cada epoch, escolhemos o conjunto de mini-lotes,
+    #e então ajustamos os parâmetros da rede em cada passagem por mini-lote.
+        for j in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [training_data[k: k + mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            mini_batches = [training_data[k: k + mini_batch_size] for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
